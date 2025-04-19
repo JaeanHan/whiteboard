97dcb95718b9ce884647f2d6a4deab0d13c8f448
JaeAnHan <jaean1999@naver.com>
2025-04-20 02:07:13 +0900

    feat: 토스트 메세지

--- src/components/ToastMessage.js
+++ src/components/ToastMessage.js
@@ -1,6 +1,6 @@
-import { useState } from "react";
-import { MessageQueue } from "../utils/MessageQueue";
-import { sideBarWidth } from "./Sidebar/SideBar";
+import { useState } from 'react';
+import { MessageQueue } from '../utils/MessageQueue';
+import { sideBarWidth } from './Sidebar/SideBar';
 
 export const ToastMessage = ({ message }) => {
   const MD = MessageQueue.getInstance();
@@ -9,17 +9,18 @@ export const ToastMessage = ({ message }) => {
   return (
     <div
       style={{
-        position: "absolute",
-        zIndex: "100",
+        position: 'absolute',
+        zIndex: '100',
         width: 400,
         height: 50,
         top: currentQueue.length * 50,
         left: sideBarWidth + 50,
         // color: "dodgerblue",
-        background: "linear-gradient(90deg, #F6F8FB 60%, #E1E9F2 100%)",
+        background: 'linear-gradient(90deg, #F6F8FB 60%, #E1E9F2 100%)',
         borderRadius: 20,
-        textAlign: "center",
-        transition: "0.3",
+        textAlign: 'center',
+        lineHeight: '50px',
+        transition: '0.3',
       }}
     >
       {message}
